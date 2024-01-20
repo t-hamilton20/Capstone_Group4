9857e3199f59bc63e144dd5135f968ba8f6918c6
t-hamilton20 <18tjwh@queensu.ca>
2024-01-20 13:10:50 -0500

    added 16x data augmentation

--- test_parsing.py
+++ test_parsing.py
@@ -3,17 +3,17 @@ import os
 
 from PIL import Image
 
-dsDir = 'data/Sign_Dataset/'
-train_folder = 'val'
-annotations_folder = 'annotations'
-output_dir = 'data/Extracted_Images/'+train_folder
+dsDir = 'data/Complete/Images/'
+train_folder = ''
+annotations_folder = 'data/Complete/mtsd_v2_fully_annotated/annotations'
+output_dir = 'data/Complete/augmented/'
 
 # Create a subfolder for extracted images if it doesn't exist
 if not os.path.exists(output_dir):
     os.makedirs(output_dir)
 
-annotation_files = [os.path.join(dsDir, annotations_folder, file) for file in
-                    os.listdir(os.path.join(dsDir, annotations_folder)) if file.endswith('.json')]
+annotation_files = [os.path.join(annotations_folder, file) for file in
+                    os.listdir(os.path.join(annotations_folder)) if file.endswith('.json')]
 
 index = 0
 labels = []
@@ -56,23 +56,28 @@ with open(annotation_file_path, 'w') as f:
                 ymax = max(ymaxTemp, yminTemp)
                 xmin = min(xminTemp, xmaxTemp)
                 ymin = min(yminTemp, ymaxTemp)
+                step_x = 4
+                step_y = 4
+                
+                for row in range(step_x):
+                    for col in range(step_y):
 
-                # Extract the base filename without the directory
-                base_filename = os.path.basename(fileName)
+                        # Extract the base filename without the directory
+                        base_filename = os.path.basename(fileName)
 
-                # Print or process the extracted information as needed
-                img_file = os.path.basename(fileName) + ".jpg"
-                label_index = labels.index(label)
-                entry = f"{base_filename}_{i}.jpg  Class: {label_index}  Coordinates: ({xmin},{ymin},{ymax},{xmax})"
-                print(entry, file=f)
+                        # Print or process the extracted information as needed
+                        img_file = os.path.basename(fileName) + ".jpg"
+                        label_index = labels.index(label)
+                        entry = f"{base_filename}_{i}_{row}_{col}.jpg  Class: {label_index}  Coordinates: ({xmin+row*2},{ymin+col*2},{ymax+col*2},{xmax+row*2})"
+                        print(entry, file=f)
 
-                # Crop the region from the full image
-                extracted_region = full_image.crop((xmin, ymin, xmax, ymax))
+                        # Crop the region from the full image
+                        extracted_region = full_image.crop((xmin+row*2, ymin+col*2, xmax+row*2, ymax+col*2))
 
-                # Save the extracted region to the subfolder
-                extracted_image_path = os.path.join(output_dir, f"{base_filename}_{i}.jpg")
-                extracted_region.save(extracted_image_path)
-                i += 1
+                        # Save the extracted region to the subfolder
+                        extracted_image_path = os.path.join(output_dir, f"{base_filename}_{i}.jpg")
+                        extracted_region.save(extracted_image_path)
+                        i += 1
 
 labels_file_path = os.path.join(output_dir, 'labels.txt')
 with open(labels_file_path, 'w') as f:

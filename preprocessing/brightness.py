fe98f09e542e754606984cd9470251f376f8a0fe
t-hamilton20 <18tjwh@queensu.ca>
2024-02-18 15:45:05 -0500

    writes to correct path for brightness

--- preprocessing/brightness.py
+++ preprocessing/brightness.py
@@ -2,17 +2,19 @@ import os
 import cv2
 
 def determine_brightness(image_path):
+    # Return dark if 0, bright if 1
+
     # Read the image
     image = cv2.imread(image_path)
     if image is None:
-        raise ValueError("Image not found or unable to load.")
+        raise ValueError(f"Image {image_path} not found or unable to load.")
 
     # Convert the image to grayscale
     gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
     # Calculate the average pixel intensity
     average_intensity = cv2.mean(gray_image)[0]
-    print("AVERAGE INTESITY: ", average_intensity)
+    # print("AVERAGE INTESITY: ", average_intensity)
 
     if average_intensity > 95:
         return 1
@@ -23,7 +25,7 @@ def adjust_brightness(image_path, brightness_factor):
     # Read the image
     image = cv2.imread(image_path)
     if image is None:
-        raise ValueError("Image not found or unable to load.")
+        raise ValueError(f"Image {image_path} not found or unable to load.")
 
     # Convert the image to HSV color space
     hsv_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
@@ -39,20 +41,26 @@ def adjust_brightness(image_path, brightness_factor):
 def generate_all_brightnesses(image_path):
     brightness_category = determine_brightness(image_path)
     # file_name, file_extension = os.path.splitext(image_path)
+    
     file_name = os.path.basename(image_path)
     file_name_without_extension, file_extension = os.path.splitext(file_name)
+    directory_path = os.path.dirname(image_path)
 
     if brightness_category == 0:
-        print("DARK")
     # Dark -> Bright
-        brightened_image = adjust_brightness(image_path, 2.0)
-        cv2.imwrite(file_name_without_extension + "_bright" + file_extension, brightened_image)
+        brightened_image = adjust_brightness(image_path, 1.5)
+        new_file_name = file_name_without_extension + "_bright" + file_extension
+        new_file_path = os.path.join(directory_path, new_file_name)
+        cv2.imwrite(new_file_path, brightened_image)
 
     else:
-        print("BRIGHT")
     # Bright -> Dark
         darkened_image = adjust_brightness(image_path, 0.5)
-        cv2.imwrite(file_name_without_extension + "_dark" + file_extension, darkened_image)
+        new_file_name = file_name_without_extension + "_dark" + file_extension
+        new_file_path = os.path.join(directory_path, new_file_name)
+        cv2.imwrite(new_file_path, darkened_image)
+    
+    return brightness_category, new_file_name
 
-image_path = 'data/Complete/augmented_9_no_small/_GcRTRDQiYyn0bQzdByW7A_0_0_1.jpg'
-generate_all_brightnesses(image_path)
+# image_path = 'data/Complete/augmented_9_no_small/_GcRTRDQiYyn0bQzdByW7A_0_0_1.jpg'
+# generate_all_brightnesses(image_path)
